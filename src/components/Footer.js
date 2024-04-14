6224a29347f57f0d5d39ba9368cd9450ecf6d352
Naveen Tata <97723849+inaveentata@users.noreply.github.com>
2024-04-14 22:45:31 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="max-w-screen-xl lg:mx-auto p-4 text-center bg-sky-400">
-      Made by Naveen. &copy; 2022 All rights reserved
+      Made by Naveen. &copy; 2024 All rights reserved
     </footer>
   );
 }
