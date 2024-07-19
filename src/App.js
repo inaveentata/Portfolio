ec105ee5b98aca619a17b78023e968ec09799632
Naveen Tata <97723849+inaveentata@users.noreply.github.com>
2024-07-19 09:13:59 +0530

    Updating portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Projects from "./components/Projects";
-import Resume from "./components/Resume";
-import Skills from "./components/Skills";
-
-//Demo portfolio: http://exill.dk/demo/codex/template/snow.html#home 
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <main>
-        <About />
-        <Projects />
-        <Skills />
-        <Contact />
-        <Resume />
-      </main>
-      <Footer />
-    </>
+      <Home />
   );
 }
 
---
+++ src/components/About/About.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Naveen Tata</strong>. I am a dedicated frontend developer with over 1.5 years of professional experience. 
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+           My expertise lies in JavaScript, React, Next.js, and Redux Toolkit, with a strong focus on building scalable and efficient web applications. I have successfully led the development of two enterprise applications from inception to deployment, leveraging my skills in frontend technologies and my ability to collaborate effectively within multidisciplinary teams. Passionate about creating intuitive user interfaces and optimizing performance, I am committed to delivering high-quality solutions that meet both user needs and business objectives.
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and cross-platform mobile development.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
